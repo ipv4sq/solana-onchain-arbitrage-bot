5aa3e38f1c92dd12d39afb300fb84c364f874c6e
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-08-12 12:33:35 +0800

    fine

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,6 +1,8 @@
 pub mod addresses;
 pub mod helpers;
+pub mod utils;
 
 // Re-export commonly used items
 pub use addresses::*;
 pub use helpers::*;
+pub use utils::*;
---
+++ src/constants/utils.rs
@@ -0,0 +1,13 @@
+use anyhow::anyhow;
+use solana_program::pubkey::Pubkey;
+use solana_sdk::account::Account;
+
+pub fn expect_owner(account_address: &Pubkey, account: &Account, expected: &Pubkey) -> anyhow::Result<()> {
+    if account.owner != *expected {
+        return Err(anyhow!(
+            "Owner mismatch for account {account_address}: expected {expected}, got {}",
+            account.owner
+        ));
+    }
+    Ok(())
+}
