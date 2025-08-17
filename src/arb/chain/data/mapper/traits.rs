610ed0d3f4d31ad5f0157ceb8fe3f7bda990e99a
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-08-17 08:56:20 +0800

    f

--- src/arb/chain/data/mapper/traits.rs
+++ src/arb/chain/data/mapper/traits.rs
@@ -1,9 +1,6 @@
-use anyhow::Result;
 use crate::arb::chain::data::Transaction;
-use crate::arb::chain::data::instruction::Instruction;
-use crate::arb::chain::types::SwapInstruction;
+use anyhow::Result;
 
 pub trait ToUnified {
     fn to_unified(&self) -> Result<Transaction>;
 }
-
