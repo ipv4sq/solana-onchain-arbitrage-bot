50672125aff975894e981290b5734d56cfb099dd
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-09-01 23:28:20 +0800

    WIP: master

--- src/arb/global/state/token_balance_holder.rs
+++ src/arb/global/state/token_balance_holder.rs
@@ -23,9 +23,9 @@ static LongTermCache: Lazy<TtlLoadingCache<(Pubkey, MintAddress), TokenAmount>> 
 #[allow(non_upper_case_globals)]
 pub static QueryRateLimiter: Lazy<Arc<RateLimiter>> = lazy_arc!({
     RateLimiter::new(
-        35,
+        60,
         Duration::from_secs(1),
-        40,
+        70,
         "AccountBalanceQueryRateLimiter".to_string(),
     )
 });
