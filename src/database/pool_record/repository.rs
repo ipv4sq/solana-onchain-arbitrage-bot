5f6ed3bde0b4468db1f2da9455d756cfec92cb50
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-09-11 15:26:40 +0800

    WIP: master

--- src/database/pool_record/repository.rs
+++ src/database/pool_record/repository.rs
@@ -1,18 +1,11 @@
-#![allow(non_upper_case_globals)]
-
 use crate::database::columns::PubkeyTypeString;
-use crate::database::pool_record::converter::build_model;
+use crate::database::pool_record::cache::{MINT_TO_POOLS, POOL_CACHE, POOL_RECORDED};
 use crate::database::pool_record::model::{
     self, Entity as PoolRecordEntity, Model as PoolRecord, Model,
 };
 use crate::global::client::db::get_db;
-use crate::global::state::any_pool_holder::AnyPoolHolder;
 use crate::util::alias::{MintAddress, PoolAddress};
-use crate::util::cache::loading_cache::LoadingCache;
-use crate::util::cache::persistent_cache::PersistentCache;
-use crate::util::structs::cache_type::CacheType;
 use anyhow::Result;
-use once_cell::sync::Lazy;
 use sea_orm::sea_query::OnConflict;
 use sea_orm::{
     ActiveValue::{NotSet, Set},
@@ -23,73 +16,6 @@ use std::collections::HashSet;
 use tracing::error;
 
 pub struct PoolRecordRepository;
-
-static MINT_TO_POOLS: Lazy<PersistentCache<MintAddress, HashSet<PoolRecord>>> = Lazy::new(|| {
-    PersistentCache::new_with_custom_db(
-        CacheType::Custom("mint_to_pools".to_string()),
-        1_000_000,
-        i64::MAX,
-        |_mint: MintAddress| async move { None },
-        Some(|mint_str: String| async move {
-            if let Ok(mint) = mint_str.parse::<Pubkey>() {
-                PoolRecordRepository::find_by_any_mint(&mint)
-                    .await
-                    .ok()
-                    .map(|pools| pools.into_iter().collect())
-            } else {
-                None
-            }
-        }),
-        Some(|_mint_str: String, _pools: HashSet<PoolRecord>, _ttl: i64| async move {}),
-    )
-});
-
-static POOL_CACHE: Lazy<PersistentCache<PoolAddress, PoolRecord>> = Lazy::new(|| {
-    PersistentCache::new_with_custom_db(
-        CacheType::Custom("pool_cache".to_string()),
-        1_000_000,
-        i64::MAX,
-        |addr: PoolAddress| {
-            let addr = addr;
-            async move {
-                let config = AnyPoolHolder::get(&addr).await?;
-                build_model(config).await.ok()
-            }
-        },
-        Some(|addr_str: String| async move {
-            if let Ok(addr) = addr_str.parse::<Pubkey>() {
-                PoolRecordEntity::find()
-                    .filter(model::Column::Address.eq(PubkeyTypeString::from(addr)))
-                    .one(get_db().await)
-                    .await
-                    .ok()
-                    .flatten()
-            } else {
-                None
-            }
-        }),
-        Some(|_addr_str: String, record: PoolRecord, _ttl: i64| async move {
-            let _ = PoolRecordRepository::upsert_pool(record).await;
-        }),
-    )
-});
-
-static POOL_RECORDED: Lazy<LoadingCache<PoolAddress, bool>> = Lazy::new(|| {
-    LoadingCache::new(1_000_000, |addr: &PoolAddress| {
-        let addr = *addr;
-        async move {
-            Some(
-                PoolRecordEntity::find()
-                    .filter(model::Column::Address.eq(PubkeyTypeString::from(addr)))
-                    .one(get_db().await)
-                    .await
-                    .ok()
-                    .flatten()
-                    .is_some(),
-            )
-        }
-    })
-});
 
 // cache related
 impl PoolRecordRepository {
@@ -107,7 +33,7 @@ impl PoolRecordRepository {
 }
 
 impl PoolRecordRepository {
-    async fn upsert_pool(pool: PoolRecord) -> Result<PoolRecord> {
+    pub(crate) async fn upsert_pool(pool: PoolRecord) -> Result<PoolRecord> {
         let db = get_db().await;
         let active_model = model::ActiveModel {
             address: Set(pool.address.clone()),
