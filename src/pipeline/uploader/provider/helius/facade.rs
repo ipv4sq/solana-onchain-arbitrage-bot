cd3afbc0245f655c26564d502c5be3206033c1d0
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-09-12 22:39:09 +0800

    WIP: master

--- src/pipeline/uploader/provider/helius/facade.rs
+++ src/pipeline/uploader/provider/helius/facade.rs
@@ -1,21 +1,40 @@
 #![allow(non_upper_case_globals)]
-use crate::convention::chain::instruction::Instruction;
-use crate::pipeline::uploader::provider::helius::client::HeliusClient;
+use crate::pipeline::uploader::provider::helius::client::{HeliusClient, HELIUS_TIP_ACCOUNTS};
 use crate::unit_ok;
 use crate::util::alias::AResult;
+use crate::util::random::random_choose;
+use crate::util::traits::pubkey::ToPubkey;
 use once_cell::sync::Lazy;
 use parking_lot::RwLock;
+use solana_program::instruction::Instruction;
+use solana_program::pubkey::Pubkey;
+use solana_program::system_instruction::transfer;
+use solana_sdk::native_token::LAMPORTS_PER_SOL;
 use solana_sdk::transaction::VersionedTransaction;
 use std::sync::Arc;
 use tokio::time::{interval, Duration};
 use tracing::error;
 
-pub fn build_helius_jito_tip_ix() -> (Vec<Instruction>, f64) {
-    todo!()
+pub fn build_helius_jito_tip_ix(payer: &Pubkey) -> (Vec<Instruction>, f64) {
+    let tip_account = random_choose(&HELIUS_TIP_ACCOUNTS).to_pubkey();
+    let minimum_tip = 0.001;
+    let ix = transfer(
+        &payer,
+        &tip_account,
+        (minimum_tip * LAMPORTS_PER_SOL as f64) as u64,
+    );
+    (vec![ix], minimum_tip)
 }
 
-pub fn build_helius_swqos_tip_ix() -> (Vec<Instruction>, f64) {
-    todo!()
+pub fn build_helius_swqos_tip_ix(payer: &Pubkey) -> (Vec<Instruction>, f64) {
+    let tip_account = random_choose(&HELIUS_TIP_ACCOUNTS).to_pubkey();
+    let minimum_tip = 0.0005;
+    let ix = transfer(
+        &payer,
+        &tip_account,
+        (minimum_tip * LAMPORTS_PER_SOL as f64) as u64,
+    );
+    (vec![ix], minimum_tip)
 }
 
 pub async fn send_helius_swqos(tx: &VersionedTransaction) -> AResult<()> {
