64224a19fe07aaea2439ae7ce27821a9aa4032f6
ipv4sq <146745120+ipv4sq@users.noreply.github.com>
2025-09-13 15:07:36 +0800

    WIP: master

--- src/dex/raydium_cl_amm/ix_account.rs
+++ src/dex/raydium_cl_amm/ix_account.rs
@@ -1,5 +1,18 @@
+use crate::dex::meteora_damm_v2::pool_data::MeteoraDammV2PoolData;
+use crate::util::alias::AResult;
+use crate::util::traits::account_meta::ToAccountMeta;
 use solana_program::instruction::AccountMeta;
+use solana_program::pubkey::Pubkey;
 
-pub struct RaydiumCammIxAccount {
-    pub payer: AccountMeta,
+#[derive(Debug, Clone)]
+pub struct RaydiumCammIxAccount {}
+
+impl RaydiumCammIxAccount {
+    async fn build_accounts_no_matter_direction_size(
+        payer: &Pubkey,
+        pool: &Pubkey,
+        pool_data: &MeteoraDammV2PoolData,
+    ) -> AResult<RaydiumCammIxAccount> {
+        todo!()
+    }
 }
--- src/dex/raydium_cl_amm/mod.rs
+++ src/dex/raydium_cl_amm/mod.rs
@@ -2,4 +2,5 @@ mod best_effort;
 mod config;
 mod ix_account;
 mod ix_data;
+mod misc;
 mod pool_data;
